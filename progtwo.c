115993b22b463334a50a90bf3b94616e16b691d5
Lucas Frias <lucas@macbookpro.lan>
2026-03-08 17:38:31 -0500

    Created fully functioning push pop and peek

--- progtwo.c
+++ progtwo.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdbool.h>
-
-//This is an exercise to recreate every data structure I possibly can 
+#include <stdlib.h>
+//This is an exercise to recreate every data structure I possibly can
 //from EECS 268 at KU into C for future C development
-//This will contain implementations of: 
+//This will contain implementations of:
 //Node, Stack, Queue, LinkedList, BinaryTree, Heap
 //Why not make this in C++ because of real classes??
 //This is an exercise and I wanna challenge myself
@@ -11,39 +11,55 @@
 
 //NODE
 typedef struct Node {
-struct Node* next;
+struct Node *next;
 int entry;
 } Node;
 
 //STACK
 typedef struct Stack{
-	Node* head;
+	Node *head;
 } Stack;
+//stack push- pushes a new value to the top
+//of the given stack
 
-//STACK METHODS
-//we're using dynamically allocated stacks
-//i get why people built these now because malloc
-//and free and memory safe c is so hard
-//but linked objects get dynamically put in the stack
-
-Node* Stack_peek_front(Stack *  user_stack){
-	return user_stack->head;
+void stack_init(Stack *mystack){
+    //clears the memory out on the head. not needed
+    //but needed because otherwise the first value is garbage
+    mystack->head = NULL;
 }
-
-void Stack_push(Stack * user_stack, int entryValue) {
-	Node userNode;
-	userNode.entry = entryValue;
-	userNode.next = user_stack->head;
-	user_stack->head = *userNode;
+void stack_push(Stack *mystack, Node *nodeToPush) {
+    //first get the head and set it to the previous head
+    Node *previousHead = mystack->head;
+    mystack->head = nodeToPush;
+    nodeToPush->next = previousHead;
 }
-
-
+int stack_peek(Stack *mystack) {
+    //returns the top value
+    if (mystack->head != NULL){
+        //this if check avoids a segmentation fault
+        return *mystack->head->entry;
+    }
+    else{
+        exit(1);
+    }
+}
+Node stack_pop(Stack *mystack){
+    //deletes the stuff from the stack!!
+    Node *previousHeadNode = mystack->head;
+    mystack->head = mystack->head->next;
+    return *previousHeadNode;
+}
 
 int main(){
-	Node firstNode;
-	Node secondNode;
-	firstNode.next = &secondNode;
-	printf("%d", firstNode.next);
+    Stack babysFirstStack;
+    Node firstNode;
+    Node secondNode;
+    firstNode.entry = 1;
+    secondNode.entry = 3;
+    stack_push(&babysFirstStack,&firstNode);
+    stack_push(&babysFirstStack, &secondNode);
+    printf("%d", stack_peek(&babysFirstStack));
+    stack_pop(&babysFirstStack);
+    printf("%d", stack_peek(&babysFirstStack));
 	return 0;
 }
-
