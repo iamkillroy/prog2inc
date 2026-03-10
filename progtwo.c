f22edf77044b6a373d79494c8fda4d3e5f686ec5
Lucas Frias <lucas@Lucass-MacBook-Pro.local>
2026-03-10 15:37:51 -0500

    Added almost functioning queue

--- progtwo.c
+++ progtwo.c
@@ -16,13 +16,28 @@ typedef struct Stack {
     uint16_t len;
 } Stack;
 
+typedef struct Queue {
+    Node * head;
+    Node * tail;
+    uint16_t len;
+} Queue;
+
+
 void handleNullPtr(void * ptr);
+
 Node * node_init(uint32_t);
+
 Stack* stack_init();
 void stack_push(Stack *, uint32_t);
 void stack_pop(Stack *);
 bool stack_is_empty(Stack *);
 uint32_t stack_peek(Stack * );
+
+Queue* queue_init();
+void queue_enqueue(Queue *, uint32_t);
+void queue_dequeue(Queue *);
+uint32_t queue_peek(Queue *);
+
 
 
 void handleNullPtr (void * ptr){
@@ -101,9 +116,67 @@ bool stack_is_empty(Stack * mystack) {
     if (mystack->len == 0){return true;}return false;
 }
 
+Queue * queue_init(){
+    Queue * myQueue = (Queue * ) malloc(sizeof(Queue));
+    myQueue->head = NULL;
+    myQueue->tail = NULL;
+    myQueue->len = 0;
+    return myQueue;
+}
 
+void queue_enqueue(Queue* myqueue, uint32_t value) {
+    ///adds a value to the back of the queue
+    // make a new node with that value
+    Node * newTailNode = malloc(sizeof(Node));
+    newTailNode->entry = value;
+    // then let's set that equal to the tail
+    //first though if the len of queue == 0 then
+    // we would be doing null pointer stuff so
+    // let's check first bestie
+    if (myqueue->len == 0){
+        //set both the head and tail to the
+        // first value, this will get replaced the second
+        // time that we enque so that head = first enqued and
+        // tail = latest enqued
+        myqueue->head = newTailNode;
+        myqueue->tail = newTailNode;
+        myqueue->len++;
+        return;
+    }
+    //okay now there should be at least some pointer here
+    myqueue->tail->next = newTailNode;
+    myqueue->tail = newTailNode;
+    myqueue->len++;
+    return;
+}
+void queue_dequeue(Queue * myqueue){
+    //removes the value stored in the queue
+    // and then evaluate the data
+    if (myqueue->len == 0){
+        return;
+    }
+    Node * prevHead = myqueue->head;
+    myqueue->head = myqueue->head->next;
+    free(prevHead);
+    return;
+}
+uint32_t queue_peek(Queue * myqueue){
+    if (myqueue->len == 0){
+        printf("Cannot peek empty queue");
+        exit(1);
+    }
+    return myqueue->head->entry;
+}
 
 
 int main(){
+    Queue * mq = queue_init();
+    queue_enqueue(mq, 32);
+    printf("%d\n", queue_peek(mq));
+    queue_enqueue(mq, 2345) ;
+    queue_enqueue(mq, 19);
+    printf("%d\n", queue_peek(mq));
+    queue_dequeue(mq);
+    printf("%d\n", queue_peek(mq));
     return 0;
 }
